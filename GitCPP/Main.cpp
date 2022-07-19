13fff1f49668a055bce1e94edcd7f489772ca326
novakvova <novakvova@gmail.com>
2022-07-19 18:42:27 +0300

    Begin menu site

--- GitCPP/Main.cpp
+++ GitCPP/Main.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 int main()
 {
+	int action;
+	cout << "1. Add new Student\n";
+	cin >> action;
+	switch (action) {
+	case 1: {
+		cout << "Add new Student\n";
+		break;
+	}
+		
+	}
 	cout << "Hello\n";
 	return 0;
 }
